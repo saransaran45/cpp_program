90448473d359a292749981c1bfead7f76be5f6bb
saravana0502 <your@example.com>
2023-10-18 07:18:04 +0530

    sorting folder added

---
+++ sorting/selectionsort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+        if (min_index != i)
+        {
+            swap(arr[i], arr[min_index]);
+        }
+    }
+}
+
+void printArr(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int arr[] = {28, 3, 15, 68, 53, 55, 4, 15};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    selectionSort(arr, len);
+    printArr(arr, len);
+    return 0;
+}
